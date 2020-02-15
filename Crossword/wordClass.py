451e48af0526602b46fdd004383a2a8c9bee15db
SeanSwayze <35567585+SeanSwayze@users.noreply.github.com>
2020-02-15 11:03:08 -0500

    Update wordClass.py
    Added world index and pointer to char index

--- Crossword/wordClass.py
+++ Crossword/wordClass.py
@@ -1,8 +1,9 @@
 class word:
 	
-	def __init__(self, length, constrained, setChars):
-		self.length = length								# Length of the word
-		self.constrained = constrained						# The number of characters linked to other characters
-		self.set = setChars									# The number of characters already set
-		self.chars = ["" for x in range(length+1)] 			# A position for each character in the word + 1 initial blank to allow easy use with depth
-		self.pointers = [None for x in range(length+1)]		# Pointers to other word who ith character is linked to word's ith character
+	def __init__(self, length, constrained, setChars, index):
+		self.index = index
+        	self.length = length								    # Length of the word
+		self.constrained = constrained						    # The number of characters linked to other characters
+		self.set = setChars									    # The number of characters already set
+		self.chars = ["" for x in range(length+1)] 			    # A position for each character in the word + 1 initial blank to allow easy use with depth
+		self.pointers = [(None, 0) for x in range(length+1)]	# Pointers to other word who ith character is linked to word's ith character (pointer to word, index in word)
