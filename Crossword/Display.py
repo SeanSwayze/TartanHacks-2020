6475dffd5d325b9f4e801663f542571103dc5f3c
SeanSwayze <35567585+SeanSwayze@users.noreply.github.com>
2020-02-15 15:30:19 -0500

    Update Display.py
    Updated

--- Crossword/Display.py
+++ Crossword/Display.py
@@ -1,47 +1,16 @@
 from tkinter import *
 import math, string
+from wordClass import *
 
 root = Tk()
 root.title = "Crosswords"
 
 canvas = Canvas(root, width=1200, height=800, bg = "white")
-canvas.grid(column = 2, row = 0, rowspan=15)
+canvas.grid(column = 2, row = 0, rowspan=40)
 
 canvasHeight = 700
 canvasWidth = 1020
 sideBarWidth = 130
-
-class Word:
-    def __init__(self, length, constrained, setChars, index):
-        self.index = index
-        self.length = length
-        self.constrained = constrained
-        self.set = setChars
-        self.chars = ["" for x in range(length+1)]
-        self.pointers = [(None, 0) for x in range(length+1)]
-    def __repr__(self):
-        string = f"""{self.chars[1:]}, Word {self.index}, Length {self.length}, {self.constrained} constraints with {self.set} set characters"""
-        return string
-
-    def showPointers(self):
-        print(self.pointers)
-
-    def setIndex(self,index):
-        self.index = index
-    def setLength(self,length):
-        self.length = length
-    def setConstrained(self,constrained):
-        self.constrained = constrained
-    def setSetChars(self,setChars):
-        self.set = setChars
-    def setChar(self,index,char):
-        self.chars[index] = char
-    def setChars(self,chars):
-        self.chars = chars
-    def setPointer(self,index,pointer):
-        self.pointers[index] = pointer
-    def setPointers(self,pointers):
-        self.pointers = pointers
     
 def isStart(board, x, y):
     result = [0,0]
@@ -84,25 +53,28 @@ def wordsIntersect(word1, word2):
     down = (word1 if word1[2]==1 else word2)
     return (right[1] in range(down[1],down[1]+len(down[3]))) and (down[0] in range(right[0],right[0]+len(right[3])))
 
-def convertWordsToClass(words):
+def convertWordsToClass(words, locations):
     index = 0
     classes = []
     for i in words:
         setChars = len(i[3])-i[3].count("-")
+        locations[index] = [i[0],i[1],i[2]]
         classes.append(Word(len(i[3]),0,setChars,index))
         classes[index].setChars([""]+["" if c == "-" else c for c in i[3]])
         for j in words:
             if (i != j) and wordsIntersect(i,j): 
-                classes[index].setConstrained(classes[index].constrained + 1)
+                classes[index].setConstrained(classes[index]._constrained + 1)
         index+=1
     for i in range(len(words)):
         for j in range(len(words)):
             if words[i] != words[j] and wordsIntersect(words[i],words[j]):
                 if words[i][2]==0:
-                    classes[i].setPointer(words[j][0]-words[i][0]+1,(classes[j],words[i][1]))
+                    classes[i].setPointer(words[j][0]-words[i][0]+1,classes[j])
+                    classes[i].setIndices(words[j][0]-words[i][0]+1,words[i][1])
                 else:
-                    classes[i].setPointer(words[j][1]-words[i][1]+1,(classes[j],words[i][0]))
-    return classes
+                    classes[i].setPointer(words[j][1]-words[i][1]+1,classes[j])
+                    classes[i].setIndices(words[j][1]-words[i][1]+1,words[i][0])
+    return classes,locations
     
 class wordBoard():
     def __init__(self,width,height,root,canvas,margins = 50):
@@ -113,6 +85,7 @@ class wordBoard():
         self.selected = None
         self.fills = [[0]*width for i in range(height)] #The fill of each square
         self.fillIds = [[None]*width for i in range(height)] #The ids of the text
+        self.fillIds2 = [[None]*width for i in range(height)] #The ids of the numbers
         self.letters = [["-"]*width for i in range(height)] #The grid of letters
         self.elements = [[None]*width for i in range(height)] #The ids of every square
         self.ids = [] #Every object in the canvas in no particular order
@@ -122,12 +95,13 @@ class wordBoard():
         self.canvas.bind("<Key>", self.canvasOnKeyPress)
         self.canvas.bind("<BackSpace>", self.canvasOnDelete)
         self.margins = margins
+        self.locations = {}
         self.xMargins = 0
         self.yMargins = 0
         self.cellDims = 100
         self.submitButton = Button(self.root, text = "Solve", width = 10,
                                 bg = "grey", command = self.activateSolve)
-        self.submitButton.grid(column=0,row = 5, columnspan = 2)
+        self.submitButton.grid(column=0,row = 25, columnspan = 2)
 
     def drawBoard(self):
         baseCellWidth = min(100,(canvasWidth-2*self.margins)//self.width)
@@ -159,17 +133,6 @@ class wordBoard():
                 self.canvas.itemconfig(self.selected, outline = "black")
                 self.selected = self.elements[row][col]
                 self.canvas.itemconfig(self.selected, outline = "red")
-                #self.canvas.delete(self.elements[row][col])
-                #self.elements[row][col] = canvas.create_rectangle(self.xMargins+col*self.cellDims, 
-                #                               self.yMargins+row*self.cellDims,
-                #                               self.xMargins+(col+1)*self.cellDims, 
-                #                               self.yMargins+(row+1)*self.cellDims,
-                #                               width = max(1,min(3,self.cellDims//20)),
-                #                               outline = "red", 
-                #                               fill = ["white","black"][self.fills[row][col]])
-                #if self.fillIds[row][col] != None:
-                #    self.addLetter(self.canvas.itemcget(self.elements[row][col],"text"),row,col)
-                #sself.selected = self.elements[row][col]
 
     def canvasOn2click(self,event):
         if ((event.x in range(self.xMargins,self.cellDims*self.width+self.xMargins)) and 
@@ -180,11 +143,13 @@ class wordBoard():
                                    fill = ("white" if self.canvas.itemcget(self.elements[row][col], "fill") == "black" else "black"))
             self.fills[row][col] = 1-self.fills[row][col]
     
+    #Finds the row and column of a square given an id
     def findCoords(self, ID):
         for row in range(len(self.elements)):
             if ID in self.elements[row]:
                 return row,self.elements[row].index(ID)
 
+    #Adds a letter to a particular square
     def addLetter(self,char,row,col):
         if self.fillIds[row][col] != None:
             self.canvas.delete(self.fillIds[row][col])
@@ -196,13 +161,14 @@ class wordBoard():
         self.ids.append(self.fillIds[row][col])
         self.letters[row][col] = char
 
+    #Puts in letters
     def canvasOnKeyPress(self,event):
         if self.selected != None and ((event.char in string.ascii_lowercase) or (event.char in string.ascii_uppercase)):
             char = event.char
             row,col = self.findCoords(self.selected)
             self.addLetter(char,row,col)
             
-
+    #Deletes Letters
     def canvasOnDelete(self,event):
         print("delete")
         if self.selected != None:
@@ -211,18 +177,37 @@ class wordBoard():
             self.fillIds[row][col] = None
             self.letters[row][col] = "-"
 
+    #Resets the entire canvas, used when changing sizes
     def destroy(self):
         for item in self.ids:
             self.canvas.delete(item)
 
+    #Creates the list of classes used by Edward
     def activateSolve(self):
         words = convertBoardToWords(self)
         for i in words:
             print(i)
-        listOfWords = convertWordsToClass(words)
+        listOfWords,locations = convertWordsToClass(words, self.locations)
         for word in listOfWords:
             print(word)
         #send list to Edward
+        self.fillWithSolution([("cab",0),("car",1),("bad",2),("rad",3)])
+
+    def fillWithSolution(self,solution):
+        nums = [0]
+        for word in solution:
+            x,y,direction = self.locations[word[1]]
+            textSize = self.cellDims//4
+            if self.fillIds2[y][x] == None:
+                self.fillIds2[y][x] = canvas.create_text((x+0.2)*self.cellDims+self.xMargins,
+                                                    (y+0.2)*self.cellDims+self.yMargins,
+                                                    text = str(max(nums)+1),
+                                                    font = f"Times {textSize}",
+                                                    fill = "grey")
+                nums.append(max(nums)+1)
+            self.ids.append(self.fillIds2[y][x])
+            for c in range(len(word[0])):
+                self.addLetter(word[0][c],y+c*direction,x+c*(1-direction))
 
 class Space():
     def __init__(self, root, canvas):
@@ -231,18 +216,18 @@ class Space():
         self.currentBoard = None
 
         self.widthLabel = Label(self.root, text = "Width: ")
-        self.widthLabel.grid(column = 0, row = 0)
+        self.widthLabel.grid(column = 0, row = 10)
         self.widthField = Entry(self.root, text = "")
-        self.widthField.grid(column=1, row = 0)
+        self.widthField.grid(column=1, row = 10)
 
         self.heightLabel = Label(self.root, text = "Height: ")
-        self.heightLabel.grid(column = 0, row = 1)
+        self.heightLabel.grid(column = 0, row = 11)
         self.heightField = Entry(self.root, text = "")
-        self.heightField.grid(column=1, row = 1)
+        self.heightField.grid(column=1, row = 11)
 
         self.submitButton = Button(self.root, text = "Submit", width = 10,
                                 bg = "grey", command = self.createCrossword)
-        self.submitButton.grid(column=0,row = 2, columnspan = 2)
+        self.submitButton.grid(column=0,row = 12, columnspan = 2)
 
     def submitCWDims(self):
         height = self.heightField.get()
