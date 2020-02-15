a94105c9273307aa82196da751ecc055e3c5a9a0
Shin-Yi <shinyi@gmail.com>
2020-02-15 04:02:03 -0500

    electric charges

--- Body.py
+++ Body.py
@@ -1,49 +1,55 @@
 from tkinter import *
 import math
+import numpy as np
+
 
 class Body:
-    def __init__(self, canvas, color, x, y, vx, vy, size, num, space = None):
+    def __init__(self, canvas, charge, x, y, vx, vy, size, num, space = None):
         self.num = num
         self.canvas = canvas
+        color = "red" if charge == 1 else "blue"
         self.id = canvas.create_oval(x-size/2, y-size/2, x+size/2, y+size/2, fill=color)
-        self.color = color
+        self.lineID = self.canvas.create_line(x,y,x+10*vx,y+10*vy)
+        self.charge = charge
         self.selected = False
-        self.x = x
-        self.y = y
-        self.vx, self.vy = vx, vy
+
+        self.position = np.array([x, y], dtype='float64')
+        self.velocity = np.array([vx, vy], dtype='float64')
+        self.force = np.array([0.0, 0.0])
+        
         self.size = size
-        self.mass = 4/3*math.pi*size**3
+        self.mass = 4/3*math.pi*(size/2)**3
         self.space = space
-        self.lineID = self.canvas.create_line(self.x,self.y,self.x+10*self.vx,self.y+10*self.vy)
 
     def __repr__(self):
         return str(self.num)
 
-    def force(self,other):
-        force = self.space.G*self.mass*other.mass/((self.x-other.x)**2+(self.y-other.y)**2)**0.5
-        angle = math.atan2(other.y-self.y,other.x-self.x)
-        fx = force*math.cos(angle)
-        fy = force*math.sin(angle)
-        return fx,fy
+    @staticmethod
+    def pair_force(body_pair, space):
+        body1, body2 = body_pair
+        dpos = body2.position - body1.position
+        sign = body1.charge * body2.charge
+
+        force_mag = space.G*body1.mass*body2.mass/np.linalg.norm(dpos)
+        body1.force -= dpos/np.linalg.norm(dpos)*force_mag*sign
+        body2.force += dpos/np.linalg.norm(dpos)*force_mag*sign
 
     def move(self):
-        ax,ay = 0,0
-        for body in self.space.bodies:
-            if body.num != self.num:
-                fx,fy = self.force(body)
-                ax = fx/self.size
-                ay = fy/self.size
-        self.vx += ax
-        self.vy += ay
-        self.x += self.vx
-        self.y += self.vy
-        dx = self.x-(self.canvas.coords(self.id)[0]+self.canvas.coords(self.id)[2])/2
-        dy = self.y-(self.canvas.coords(self.id)[1]+self.canvas.coords(self.id)[3])/2
-        self.canvas.move(self.id, dx, dy)
+        acc = self.force/self.mass
+        self.velocity += acc
+        self.position += self.velocity
+        
+        self.canvas.move(self.id, self.velocity[0], self.velocity[1])
+        self.force = np.array([0.0, 0.0])
     
     def vectors(self):
         self.canvas.delete(self.lineID)
-        self.lineID = self.canvas.create_line(self.x,self.y,self.x+10*self.vx,
-                                              self.y+10*self.vy, fill = "white")
+        self.lineID = self.canvas.create_line(self.position[0],self.position[1],self.position[0]+10*self.velocity[0],
+                                              self.position[1]+10*self.velocity[1], fill = "white")
+
+    def set_color(self):
+        color = "red" if self.charge == 1 else "blue"
+        self.canvas.itemconfig(self.id, fill = color)
+
     def updateMass(self):
         self.mass = 4/3*math.pi*self.size**3
