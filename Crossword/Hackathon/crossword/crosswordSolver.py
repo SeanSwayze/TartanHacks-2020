c2d03087f17c55805ef04df61c4fe6851f8f3bc0
EdwardG <39707465+EdwardG5@users.noreply.github.com>
2020-02-15 16:12:44 -0500

    Update crosswordSolver.py

--- Crossword/Hackathon/crossword/crosswordSolver.py
+++ Crossword/Hackathon/crossword/crosswordSolver.py
@@ -18,90 +18,97 @@ def listToLifoQueue(words):
 
 def solveHelper(wordList, root):
 	
-	print("sH2")
+	#print("sH2")
 	# Base case: Success. No more words to match
 	if wordList.empty():
 		return [[]]
 	
 	# Recursive case
 	else:
-		print("sH3")
+		#print("sH3")
 		word = wordList.get(block=False) # currentWord. Next word to be filled in
-		solutions = match(word, 1, root, wordList, root)
+		solutions = match(word, 0, root, wordList, root)
 		return solutions
 
 
 def match(word, cL, node, wordList, root):
-	print("match4")
+	#print("Match start")
 	# Base case: trie matched against word
 	if cL == word.length():
-		print("match5")
+		#print("match5")
 		# Node is word
 		if node.word():
-			print("match6")
+			#print("match6")
 			# Modify node values appropriately
 			solution = node.whichWord()
 			word.setChars([""]+list(solution))
 			# Propagate changes to other connected nodes
-			print("match7")
+			#print("match7")
 			word.propagate()
-			print("match8")
+			#print("match8")
 			# Find list of solution lists
 			solutions = solveHelper(wordList, root)
-			print("match9")
+			#print("match9")
 			# Total solution found
 			if solutions:
-				print("match10")
+				#print("match10")
+				#print("HEREHHJEHRKJEHHEREHHJEHRKJEHHEREHHJEHRKJEHHEREHHJEHRKJEH!")
+				#print(solutions)
+				#print(solution)
 				for x in solutions:
 					x.append(solution)
-				print("match11")
+				#print(solutions)
+				#print("match11")
 				return solutions
 			# Collision occurs down the line
 			else:
-				print("match12")
+				#print("match12")
 				return []
 		
 		# Node is not word
 		else:
-			print("match13")
+			#print("match13")
 			return []
 	
 	# Recursive case
 	else:
-		print("matche14")
+		#print("matche14")
 		# Next character set
 		if word.set(cL): 
-			print("matche15")
+			#print("matche15")
 			# Find next starting node
 			nextNode = node.childL(word._chars[cL])
 			# No solution
 			if (nextNode == None) or (nextNode.maxLength() < word.length()): 
-				print("matche16")
+				#print("matche16")
 				return []
 			# Continue with search
 			else:
-				print("matche17")
+				#print("matche17")
 				solutions = match(word, cL+1, nextNode, wordList, root)
 				return solutions
 		
 		# Next character blank
 		else:
-			print("matche18")
+			#print("matche18")
 			# Iterate through all 26 possibilities (casing on whether or not they are long enough to be an option)
 			solutions = []
 			for x in range(26):
-				print("fl", x)
+				#print("fl", x)
 				# Find next starting node
 				nextNode = node.childN(x)
 				# No solution
 				if (nextNode == None) or (nextNode.maxLength() < word.length()): 
-					print("fli", x)
+					#print("fli", x)
 					continue
 				# Continue with search
 				else:
-					print("flie", x)
+					#print("flie", x)
+					#print("cl+1", cL+1)
+					#print(nextNode, nextNode._depth)
+					#print()
 					solutions += match(word, cL+1, nextNode, wordList, root)
-			print("matche19")
+			#print("matche19")
 			return solutions
 
 
@@ -114,8 +121,27 @@ def solve(wordList, root):
 	return solutions
 
 
-def testing():
-	#dictionary = ["a","ba","bad","abad","ad","caba","add","cad", "addda"]
+
+
+dictionary = ["a","ba","bad","abad","ad","caba","add","cad", "addda"]
+
+def testing0():
+	dictionary = ["a","ba","bad","abad","ad","caba","add","cad", "addda"]
+	root = listToTrie(dictionary)
+	word1 = Word(4, 1, 0, 1)
+	word2 = Word(3, 2, 0, 2)
+	word3 = Word(5, 1, 0, 3)
+	word1._pointers[1] = (word2, 1)
+	word2._pointers[1] = (word1, 1)
+	word2._pointers[3] = (word3, 2)
+	word3._pointers[2] = (word2, 3)
+	words = [word1, word2, word3]
+	print("Here1")
+	print(words[0]._chars)
+	solutions = solve(words, root)
+	print("Solutions:", solutions)
+
+def testing1():
 	dictionary = ["ba", "abad"]
 	root = listToTrie(dictionary)
 	#word1 = Word(4, 1, 0, 1)
@@ -132,5 +158,14 @@ def testing():
 	solutions = solve(words, root)
 	print("Solutions:", solutions)
 
+# Match testing
+def testing2():
+	dictionary = ["ba", "abad"]
+	root = listToTrie(dictionary)
+	word1 = Word(4, 0, 0, 1)
+	emptyLifoQueue = LifoQueue()
 
-testing()
+	print(match(word1, 0, root, emptyLifoQueue, root))
+
+
+#testing0()
